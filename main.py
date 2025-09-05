2808cda4834ef8537398795d5ed8e1fd0dd3fb85
VIVEK <vivekswamy021@gmail.com>
2025-09-05 18:22:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ FRONTEND_URL = "https://marvelous-madeleine-f5af11.netlify.app/"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],  # Allow only your frontend domain for security
+    allow_origins=["*"],  # Allow only your frontend domain for security
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
